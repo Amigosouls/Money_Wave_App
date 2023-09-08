7fbd30dc58479755d773c14e8d8f47caa50a7734
Amigosouls <amigosouls022@gmail.com>
2023-09-08 15:09:35 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Money_Wave_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles ="Admin")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [Authorize(Roles = "Business")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using Money_Wave_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Money_Wave_App.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        MoneyWaveEntities2 db = new MoneyWaveEntities2();
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        public ActionResult Login(string user_email, string password)
+        {
+            validateUser_Result userRole = db.validateUser(user_email, password).FirstOrDefault();
+            string msg = string.Empty;
+            if (userRole.user_id != -1)
+            {
+                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, user_email, DateTime.Now, DateTime.Now.AddMinutes(30), true, userRole.role_name, FormsAuthentication.FormsCookiePath);
+                string hash = FormsAuthentication.Encrypt(ticket);
+                HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, hash);
+                if (ticket.IsPersistent)
+                {
+                    cookie.Expires = ticket.Expiration;
+                }
+                Response.Cookies.Add(cookie);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                msg = "User Name or Password is Invalid";
+            }
+            ViewBag.Msg = msg;
+            return View();
+        }
+       
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login","Login");
+        }
+
+    }
+}
